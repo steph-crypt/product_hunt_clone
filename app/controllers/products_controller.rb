8069039cb3cf37049fea0ab04917483bae2c4ad6
Sofia <sgandsteph@gmail.com>
2020-11-12 18:24:29 +0100

    Prepare Travis CI configuration

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,6 +14,7 @@ class ProductsController < ApplicationController
     if @product.save
       redirect_to root_path
     else
+      flash[:error] = @product.errors.full_messages
       render :new
     end
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ test "full_name returns the capitalized first and last name" do
+   user = User.new(first_name: 'john', last_name: "WiLkes")
+   assert_equal "John Wilkes", user.full_name
+ end
 end
